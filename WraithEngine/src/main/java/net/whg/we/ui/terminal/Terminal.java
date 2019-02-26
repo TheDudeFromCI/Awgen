d66545977fbe8bc70753197a633fca1d0b5be921
TheDudeFromCI <thedudefromci@gmail.com>
2019-02-26 10:19:43 -0700

    Added List command.

--- WraithEngine/src/main/java/net/whg/we/ui/terminal/Terminal.java
+++ WraithEngine/src/main/java/net/whg/we/ui/terminal/Terminal.java
@@ -6,6 +6,7 @@ import net.whg.we.command.VariableKeyring;
 import net.whg.we.command.common.ClearCommand;
 import net.whg.we.command.common.ForCommand;
 import net.whg.we.command.common.HelpCommand;
+import net.whg.we.command.common.ListCommand;
 import net.whg.we.command.common.PrintCommand;
 import net.whg.we.command.common.RandomCommand;
 import net.whg.we.command.common.SetCommand;
@@ -46,6 +47,7 @@ public class Terminal extends SimpleContainer implements CommandSender
 		_commandList.addCommand(new SetCommand());
 		_commandList.addCommand(new RandomCommand());
 		_commandList.addCommand(new ForCommand());
+		_commandList.addCommand(new ListCommand());
 
 		_variables = new TerminalKeyring();
 
