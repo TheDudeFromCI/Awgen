9a64ccffe744add10e79dd0ba8493ee437589206
TheDudeFromCI <thedudefromci@gmail.com>
2019-03-24 00:12:47 -0700

    Added small patch for closing sockets freezing up the thread.

--- WraithEngine/src/main/java/net/whg/we/network/client/DefaultClient.java
+++ WraithEngine/src/main/java/net/whg/we/network/client/DefaultClient.java
@@ -15,99 +15,94 @@ import net.whg.we.utils.logging.Log;
  */
 public class DefaultClient
 {
-    private ChannelProtocol _protocol;
-    private TCPChannel _channel;
-    private Thread _thread;
-    private ClientEvent _event;
+	private ChannelProtocol _protocol;
+	private TCPChannel _channel;
+	private Thread _thread;
+	private ClientEvent _event;
 
-    /**
-     * Creates a new default client instance with the set protocol.
-     *
-     * @param protocol
-     */
-    public DefaultClient(ChannelProtocol protocol, String ip, int port)
-            throws IOException
-    {
-        _protocol = protocol;
-        _event = new ClientEvent(this);
-        _channel = new DefaultTCPChannel(new Socket(ip, port), true);
-        _protocol.init(_channel.getInputStream(), _channel.getOutputStream(),
-                _channel);
+	/**
+	 * Creates a new default client instance with the set protocol.
+	 *
+	 * @param protocol
+	 */
+	public DefaultClient(ChannelProtocol protocol, String ip, int port) throws IOException
+	{
+		_protocol = protocol;
+		_event = new ClientEvent(this);
+		_channel = new DefaultTCPChannel(new Socket(ip, port), true);
+		_protocol.init(_channel.getInputStream(), _channel.getOutputStream(), _channel);
 
-        _thread = new Thread(() ->
-        {
-            _event.onConnectToServer(_channel);
+		_thread = new Thread(() ->
+		{
+			_event.onConnectToServer(_channel);
 
-            try
-            {
-                while (true)
-                    _protocol.next();
-            }
-            catch (SocketException e)
-            {
-                Log.info("Client socket has been forcefully closed.");
-            }
-            catch (IOException e)
-            {
-                Log.info("The connection has been closed.");
-            }
-            catch (Exception e)
-            {
-                Log.errorf("An error has occured while handling client socket!",
-                        e);
-            }
-            finally
-            {
-                try
-                {
-                    _protocol.close();
-                    _channel.close();
-                    _event.onDisconnectedFromServer(_channel);
-                }
-                catch (IOException e)
-                {
-                    Log.errorf(
-                            "An error has occured while closing client sockets!",
-                            e);
-                }
-            }
-        });
-        _thread.setName("Client");
-        _thread.setDaemon(true);
-        _thread.start();
-    }
+			try
+			{
+				while (!_channel.isClosed())
+					_protocol.next();
+			}
+			catch (SocketException e)
+			{
+				Log.info("Client socket has been forcefully closed.");
+			}
+			catch (IOException e)
+			{
+				Log.info("The connection has been closed.");
+			}
+			catch (Exception e)
+			{
+				Log.errorf("An error has occured while handling client socket!", e);
+			}
+			finally
+			{
+				try
+				{
+					_protocol.close();
+					_channel.close();
+					_event.onDisconnectedFromServer(_channel);
+				}
+				catch (IOException e)
+				{
+					Log.errorf("An error has occured while closing client sockets!", e);
+				}
+			}
+		});
+		_thread.setName("Client");
+		_thread.setDaemon(true);
+		_thread.start();
+	}
 
-    public void close() throws IOException
-    {
-        if (isClosed())
-            return;
+	public void close() throws IOException
+	{
+		if (isClosed())
+			return;
 
-        _protocol.close();
-        _channel.close();
+		_protocol.close();
+		_channel.close();
 
-        try
-        {
-            if (_thread != null)
-                _thread.join();
-        }
-        catch (InterruptedException e)
-        {
-            Log.errorf("Failed to wait for client thread to close!", e);
-        }
-    }
+		try
+		{
+			if (_thread != null)
+				_thread.join(1000);
+		}
+		catch (InterruptedException e)
+		{
+			Log.errorf("Failed to wait for client thread to close!", e);
+		}
+	}
 
-    public boolean isClosed()
-    {
-        return _channel.isClosed();
-    }
+	public boolean isClosed()
+	{
+		return _channel.isClosed();
+	}
 
-    public ChannelProtocol getProtocol()
-    {
-        return _protocol;
-    }
+	public ChannelProtocol getProtocol()
+	{
+		return _protocol;
+	}
 
-    public ClientEvent getEvents()
-    {
-        return _event;
-    }
+	public ClientEvent getEvents()
+	{
+		return _event;
+	}
 }
--- WraithEngine/src/main/java/net/whg/we/network/server/DefaultServer.java
+++ WraithEngine/src/main/java/net/whg/we/network/server/DefaultServer.java
@@ -7,141 +7,134 @@ import net.whg.we.utils.logging.Log;
 
 public class DefaultServer implements Server
 {
-    private int _port;
-    private ConnectedClientList _clientList;
-    private ServerProtocol _protocol;
-    private ServerEvent _events;
-    private TCPSocket _socket;
-    private Thread _thread;
+	private int _port;
+	private ConnectedClientList _clientList;
+	private ServerProtocol _protocol;
+	private ServerEvent _events;
+	private TCPSocket _socket;
+	private Thread _thread;
 
-    public DefaultServer(ServerProtocol protocol, int port) throws IOException
-    {
-        _port = port;
-        _protocol = protocol;
-        _events = new ServerEvent(this);
-        _clientList = new ConnectedClientList();
-        _socket = new DefaultTCPSocket();
+	public DefaultServer(ServerProtocol protocol, int port) throws IOException
+	{
+		_port = port;
+		_protocol = protocol;
+		_events = new ServerEvent(this);
+		_clientList = new ConnectedClientList();
+		_socket = new DefaultTCPSocket();
 
-        _thread = new Thread(() ->
-        {
-            try
-            {
-                _socket.open(_port);
-                Log.trace("Initialized server thread.");
+		_thread = new Thread(() ->
+		{
+			try
+			{
+				_socket.open(_port);
+				Log.trace("Initialized server thread.");
 
-                _events.onServerStarted();
+				_events.onServerStarted();
 
-                while (true)
-                {
-                    Log.trace("Waiting for connection.");
+				while (!_socket.isClosed())
+				{
+					Log.trace("Waiting for connection.");
 
-                    try
-                    {
-                        TCPChannel socket = _socket.nextChannel();
+					try
+					{
+						TCPChannel socket = _socket.nextChannel();
 
-                        Log.infof(
-                                "A client has connected to the server. IP: %s",
-                                socket.getIP());
+						Log.infof("A client has connected to the server. IP: %s", socket.getIP());
 
-                        ClientConnection client =
-                                _protocol.openChannelProtocol(socket);
-                        _clientList.addClient(client);
-                        _events.onClientConnected(client);
-                    }
-                    catch (SocketException e)
-                    {
-                        Log.info("Server socket has been forcefully closed.");
-                        break;
-                    }
-                    catch (Exception e)
-                    {
-                        Log.errorf(
-                                "There has been an internal error in the server socket!",
-                                e);
-                        break;
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Log.errorf(
-                        "There has been a error while opening the server socket!",
-                        e);
+						ClientConnection client = _protocol.openChannelProtocol(socket);
+						_clientList.addClient(client);
+						_events.onClientConnected(client);
+					}
+					catch (SocketException e)
+					{
+						Log.info("Server socket has been forcefully closed.");
+						break;
+					}
+					catch (Exception e)
+					{
+						Log.errorf("There has been an internal error in the server socket!", e);
+						break;
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Log.errorf("There has been a error while opening the server socket!", e);
 
-                _events.onServerFailedToStart(port);
-            }
-            finally
-            {
-                try
-                {
-                    _socket.close();
-                    _events.onServerStopped();
-                }
-                catch (IOException e)
-                {
-                    Log.errorf("Failed to properly close server socket!", e);
-                }
-            }
-        });
-        _thread.setDaemon(true);
-        _thread.setName("Server");
-        _thread.start();
-    }
+				_events.onServerFailedToStart(port);
+			}
+			finally
+			{
+				try
+				{
+					_socket.close();
+					_events.onServerStopped();
+				}
+				catch (IOException e)
+				{
+					Log.errorf("Failed to properly close server socket!", e);
+				}
+			}
+		});
+		_thread.setDaemon(true);
+		_thread.setName("Server");
+		_thread.start();
+	}
 
-    @Override
-    public void stopServer()
-    {
-        if (_socket.isClosed())
-            return;
+	@Override
+	public void stopServer()
+	{
+		if (!isRunning())
+			return;
 
-        Log.info("Closing server thread.");
+		Log.info("Closing server thread.");
 
-        try
-        {
-            _socket.close();
-        }
-        catch (IOException e)
-        {
-            Log.errorf("Failed to close server socket!", e);
-        }
+		try
+		{
+			_socket.close();
+		}
+		catch (IOException e)
+		{
+			Log.errorf("Failed to close server socket!", e);
+		}
 
-        try
-        {
-            Log.trace("Waiting until thread is fully closed.");
-            _thread.join();
-        }
-        catch (InterruptedException e)
-        {
-            Log.errorf("Failed to wait for server thread to close!", e);
-        }
-    }
+		try
+		{
+			Log.trace("Waiting until thread is fully closed.");
+			_thread.join(1000);
+		}
+		catch (InterruptedException e)
+		{
+			Log.errorf("Failed to wait for server thread to close!", e);
+		}
+	}
 
-    @Override
-    public boolean isRunning()
-    {
-        return !_socket.isClosed();
-    }
+	@Override
+	public boolean isRunning()
+	{
+		return !_socket.isClosed();
+	}
 
-    @Override
-    public int getPort()
-    {
-        return _port;
-    }
+	@Override
+	public int getPort()
+	{
+		return _port;
+	}
 
-    @Override
-    public ConnectedClientList getClientList()
-    {
-        return _clientList;
-    }
+	@Override
+	public ConnectedClientList getClientList()
+	{
+		return _clientList;
+	}
 
-    @Override
-    public ServerEvent getEvents()
-    {
-        return _events;
-    }
+	@Override
+	public ServerEvent getEvents()
+	{
+		return _events;
+	}
 
-    public ServerProtocol getProtocol()
-    {
-        return _protocol;
-    }
+	public ServerProtocol getProtocol()
+	{
+		return _protocol;
+	}
 }
