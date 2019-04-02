36c3477ea2d6b1062b2c9b43a4782c06ed955fd7
TheDudeFromCI <thedudefromci@gmail.com>
2019-04-01 17:15:42 -0700

    Wrote some package info documents.

---
+++ WraithEngine/src/main/java/net/whg/frameworks/scene/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The the scene framework for Wraith Engine is in charge of handling all
+ * processes related to handling and processing the scene hierarchy.
+ *
+ * @author TheDudeFromCI
+ */
+package net.whg.frameworks.scene;
---
+++ WraithEngine/src/main/java/net/whg/frameworks/util/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * The utility framework is a collection of isolated classes and functions which
+ * are useful for various actions used within the other frameworks. These are
+ * designed to have no dependencies outside of this framework and are usually
+ * limited to only a few class objects or less.
+ *
+ * @author TheDudeFromCI
+ */
+package net.whg.frameworks.util;
