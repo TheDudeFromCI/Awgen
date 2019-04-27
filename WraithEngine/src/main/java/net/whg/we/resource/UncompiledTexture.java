a79628a87acaf68cb988453aa48cfb22334d1fe0
TheDudeFromCI <thedudefromci@gmail.com>
2019-04-26 17:15:00 -0700

    Added simple texture saving and loading.

--- WraithEngine/src/main/java/net/whg/we/resource/UncompiledTexture.java
+++ WraithEngine/src/main/java/net/whg/we/resource/UncompiledTexture.java
@@ -2,9 +2,15 @@ package net.whg.we.resource;
 
 import net.whg.frameworks.resource.ResourceFile;
 
+/**
+ * Represents a collection of data which can be used to constuct a texture
+ * resource.
+ *
+ * @author TheDudeFromCI
+ */
 public class UncompiledTexture
 {
 	public String name;
 	public TextureColorData colorData;
-	public ResourceFile path;
+	public transient ResourceFile path;
 }
